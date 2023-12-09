ae39c3f5d50cd69c8c95d5214b5bc6952395f097
David Felipe <felipe30.10@outlook.com>
2023-12-08 22:10:02 -0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ tasks, setTasks ] = useState([])
+  
+  async function getAllTask(){
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json())
+    return data
+
+  }
+
+  useEffect( () => {
+    getAllTask().then(res => setTasks(res))
+  },[])
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {tasks.map(todo => {
+        return (
+          <div key={todo.id}>
+            <input type="checkbox" name={todo.title} id={todo.id} checked={todo.completed}/>
+            <strong>{todo.title}</strong>
+          </div>
+        )
+      })}
     </>
+    
   )
 }
 
