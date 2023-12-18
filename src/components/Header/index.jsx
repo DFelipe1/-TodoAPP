85ecbf78d76f186b7cbfd7c854f777673fb492dd
David Felipe <felipe30.10@outlook.com>
2023-12-18 19:07:31 -0300

    add theme mode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,38 @@
 import styles from "./style.module.css";
 
 import Mon from '../../assets/Mon.svg'
+import Sun from '../../assets/Sun.svg'
+import { useState } from "react";
 
 export function Header() {
+
+    const prefersColorScheme =  window.matchMedia('(prefers-color-scheme: dark)')
+
+    const [ theme, setTheme ] = useState(prefersColorScheme)
+
+
+    const root = document.getElementsByClassName('body')[0]
+    if(theme){
+        root.classList.add('dark')
+    }
+    
+    function themeColor() {
+        setTheme(!theme)
+        root.classList.toggle('dark')
+    }
 
     return (
         <header className={styles.header}>
             <h1 className={styles.logo}>TODO</h1>
 
-            <button className={styles.themeMode}>
-                <img src={Mon} alt="Mon - Dark/ligth Mode" />
+            <button className={styles.themeMode} onClick={() => themeColor()}>
+                {
+                    theme ? (
+                        <img src={Sun} alt="Sun - Dark/ligth Mode" />
+                    ) : (
+                        <img src={Mon} alt="Mon - Dark/ligth Mode" />
+                    )
+                }
             </button>
         </header>
     )
